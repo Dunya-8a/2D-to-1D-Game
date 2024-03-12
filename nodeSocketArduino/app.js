60ef466dd0be3a441431deb6bf8b6c2c1871be8c
Dunya-8a <53619378+Dunya-8a@users.noreply.github.com>
2024-03-12 17:44:39 -0400

    Update arduino node socket dependencies and ES6 syntax

--- nodeSocketArduino/app.js
+++ nodeSocketArduino/app.js
@@ -1,38 +1,44 @@
-const http = require("http");
-const fs = require("fs");
-const index = fs.readFileSync("../index.html");
+import { createServer } from "http";
+import { readFileSync } from "fs";
+import { SerialPort } from "serialport";
+import { ReadlineParser } from "@serialport/parser-readline";
+import { Server as SocketIOServer } from "socket.io";
 
-const SerialPort = require("serialport");
-const parsers = SerialPort.parsers;
+try {
+	const index = readFileSync("../index.html");
+	const port = new SerialPort({ path: "/dev/cu.usbmodem1101", baudRate: 14400 });
+	const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
 
-const parser = new parsers.Readline({
-	delimiter: "\r\n",
-});
+	const app = createServer(function (_req, res) {
+		res.writeHead(200, { "Content-Type": "text/html" });
+		res.end(index);
+	});
 
-var port = new SerialPort("/dev/tty.wchusbserialfa1410", {
-	baudRate: 9600,
-	dataBits: 8,
-	parity: "none",
-	stopBits: 1,
-	flowControl: false,
-});
+	// Specify the CORS options
+	const io = new SocketIOServer(app, {
+		cors: {
+			origin: ["http://localhost:5500", "http://127.0.0.1:5500", "*"], // Allow only this origin to connect
+			methods: ["GET", "POST"], // Allowed request methods
+			// allowedHeaders: ["my-custom-header"], // Custom headers here
+			credentials: true, // Allow credentials (cookies, session IDs)
+		},
+	});
 
-port.pipe(parser);
+	io.on("connection", () => console.log("Node is listening to port"));
 
-var app = http.createServer(function (req, res) {
-	res.writeHead(200, { "Content-Type": "text/html" });
-	res.end(index);
-});
+	parser.on("data", (data) => {
+		try {
+			if (!data) {
+				console.log("Data not found!");
+			} else {
+				io.emit("data", data);
+			}
+		} catch (error) {
+			console.log("Error:", error);
+		}
+	});
 
-var io = require("socket.io").listen(app);
-
-io.on("connection", function (socket) {
-	console.log("Node is listening to port");
-});
-
-parser.on("data", function (data) {
-	console.log("Received data from port: " + data);
-	io.emit("data", data);
-});
-
-app.listen(3000);
+	app.listen(3000);
+} catch (error) {
+	console.log("Error:", error);
+}
